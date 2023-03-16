e90d5c88f5b18093b49580ab0bf836537454162f
ChengChengChu <85456619+ChengChengChu@users.noreply.github.com>
2023-03-16 01:42:40 -0700

    Update main_1.py

--- CNS/hw1/main_1.py
+++ CNS/hw1/main_1.py
@@ -1,12 +1,32 @@
 from pwn import *
+import time 
+import random
+import base64
+import warnings  
+warnings.filterwarnings("ignore")  
+
 
 words = []
+get_flag = False
 with open('common_words.txt', 'r') as fp :
     for line in fp.read().splitlines() :
         words.append(line)
 
+def round_3(cypher_1, plain_1, cypher_2) :
+
+    # print(base64.b64decode(cypher_1))
+    # print(plain_1.encode('utf-8'))
+    tmp = bytes(c ^ p for c, p in zip(base64.b64decode(cypher_1), base64.b64decode(cypher_2)))
+
+    # print(f"\n### cypher_1 xor cypher_2 == {tmp} ###\n")
+
+    plain_2 = bytes(c ^ p for c, p in zip(tmp, plain_1.encode())).decode()
+
+    return plain_2
+
+
 def decode_ceaser(text) :
-    for offset in range(27) :
+    for offset in range(50) :
         decode = ""
         for x in text :
             if ord(x) >= 97 and ord(x) <= 122 :
@@ -81,10 +101,25 @@ def check_valid(text) :
     else :
         return False
 
+def round_3(cypher_1, plain_1, cypher_2) :
+    # print(type(cypher_1), type(plain_1), type(cypher_2))
+    # print(cypher_1, plain_1, cypher_2)
+    # print(base64.b64decode(cypher_1))
+    # print(plain_1.encode('utf-8'))
+    tmp = bytes(c ^ p for c, p in zip(base64.b64decode(cypher_1), base64.b64decode(cypher_2)))
+
+    # print(f"\n### cypher_1 xor cypher_2 == {tmp} ###\n")
+
+    plain_2 = bytes(c ^ p for c, p in zip(tmp, plain_1.encode())).decode()
+
+    return plain_2
+
 
 def get_message(r) :
     r.recvuntil('[i]')
     tmp = r.recvline().decode('utf-8')
+   #  import pdb
+   #  pdb.set_trace()
     tmp = tmp.split('=')[-1].replace("'", '')
     tmp = tmp.split()
     if tmp[0] == ' ' and tmp[-1] == ' ' :
@@ -93,7 +128,7 @@ def get_message(r) :
         return ' '.join(tmp)
 def main() :
     # print("pass\n")
-    r = remote('cns.csie.org', 44398)
+    # r = remote('cns.csie.org', 44398)
    #  r.interactive()
     r.recvuntil("[?]" )
 
@@ -110,43 +145,104 @@ def main() :
     ########## Round_1 #########
     msg = get_message(r)
     decode = decode_ceaser(msg)
-    print(f"\n\n#### Round 1 : ####\nplain_text : {' '.join(decode.split())}\n")
-    r.sendline(' '.join(decode.split()))
+    if check_valid(decode) :
+        #print(f"\n\n#### Round 1 : ####\nplain_text : {' '.join(decode.split())}\n")
+        tmp = ' '.join([x.replace('"', '').replace('\n', '') for x in decode.split()])
+
+        print(f"\n\n### Round 1 : ###\nplain_text : {tmp}\n")
+        r.sendline(tmp)
+    else :
+        # print("####### Round 0 : not enough keywords ##########")
+        # print(f"decodec_message : {decode}")
+        # exit(0)
+        return
 
     ######## Round_2 ###########
     msg = get_message(r)
     flag = True
     # print(f"cypher text : {msg}\n")
-    for i in range(2, 80) :
+    for i in range(2, 100) :
         decode = decode_fence(msg, i)
         if check_valid(decode) :
             flag = False
-            print(f"\n\n#### Round 2 : ####\nplain_text : {' '.join(decode.split())}\n")
+            print(f"\n\n### Round 2 : ###\nplain_text : {' '.join(decode.split())}\n")
             r.sendline(' '.join(decode.split()))
             break
             #print(check_valid(decode), decode)
         #else :
         #    print("Not plain_text")
     if flag :
-        print("####### Round 2 decode fail, not enough keywords ########\n")
-        r.sendline("aaaa")
-        exit(1)
-    r.interactive()
-    for i in range(12) :
-        r.recvline()
-    r.recvuntil('[i]')
+        # print("####### Round 2 decode fail, not enough keywords ########\n")
+        # print(f"decode_message : {decode}")
+        # exit(1)
+        return
+    get_flag = True
+    # r.interactive()
+    r.recvline()
     
-
-    msg = r.recvline().decode('utf-8').replace('\n', '').split()[-1]
-    import base64
-
-    msg = msg.replace('b', '').replace("'", '').replace('=', '')
-    print(msg, '\n\n\n\n\n')
-    r.interactive()
-
+    r.recvline()
+    r.recvline()
+    
+    r.recvline()
+    r.recvline()
+    r.recvline()
+    r.recvline()
+    r.recvline()
+    r.recvline()
+    r.recvuntil('= ')
+    c_1 = r.recvline().decode().replace('\n', '').replace('b', '').replace("'", '')# .encode()
+    # print(f"c_1 : {c_1}")
+    # r.interactive() 
+    r.recvuntil('= ')
+    p_1 = r.recvline().decode().replace('\n', '').replace('"', '').replace("'", '')
+    # print(f"p_1 : {p_1}\n\n")
+    
+    r.recvuntil('= ')
+    c_2 = r.recvline().decode().replace('\n', '').replace('b', '').replace("'", '')#.encode()
+    # print(f"c_2 : {c_2}")
+    # s + b'=' * (-len(s) % 4) 
+    # r.interactive()
+    # msg = r.recvline().decode().replace('\n', '').replace('b', '').replace("'", '').encode()
+    # print(len(c_1), len(c_2))
+    # r.interactive()
+    if len(c_1) >= len(c_2) :
+        decode = round_3(c_1.encode(), p_1, c_2.encode())
+    else:
+        return
+    # print(type(msg.encode()), msg.encode(), 'fuck\n\n')
+    # msg = msg.split()[-1].replace('"', '').replace('b', '').replace("'", '')    
+    
+    # print(len(msg), '\n\n\n\n\n')
+    # import pdb
+    # pdb.set_trace() 
+    if check_valid(decode) :
+        print(f"### Round_3 plain_text : {decode} ###")
+        r.sendline(decode)
+        print("Finish Round 1, and 2 !!\n")
+        # r.interactive()
+    else :
+        return
+    
+    r.recvuntil('Hint2: You can safely ignore all white spaces and punctuation marks.')
+    # r.interactive()
+    r.recvline()
+    r.recvline()
+    r.recvuntil('= ')
+    msg = r.recvline().decode().replace('\n', '').replace("'", '')
+    # print('\n\n', msg, '\n\n')
+    msg = ' '.join([x.upper() for x in msg.split()])
+    print('\n\n\n', msg, '\n\n\n')
+    r.sendline(msg)
+    # r.interactive()
 
 
 if __name__ == "__main__" :
-    main()
+    times = 1
+    while True :
+        print(f"try {times} times.")
 
-
+        r = remote('cns.csie.org', 44398)
+        main()
+        # time.sleep(0.3)
+        times += 1
+        if get_flag : break
